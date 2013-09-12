147a3931d36bb12e02bfb51414dc454cb6a4e341
Mike Rosseel <mike@eon-consult.be>
2013-09-12 18:07:48 +0200

    initial commit, works

---
+++ randomTimer.js
@@ -0,0 +1,20 @@
+var play = require('play');
+
+var minutesToMilliseconds = 60;
+
+var minMins = 12;
+var maxMins = 17;
+
+var min = minMins * minutesToMilliseconds;
+var max = maxMins * minutesToMilliseconds;
+
+var fn = function(runFunction) {
+	var timeout = Math.round((max-min)*Math.random()+min);
+	setTimeout((function() {
+		play.sound('./blip.wav');		
+	  	console.log('sleeping for ' + timeout);
+	  	runFunction(runFunction);
+	}), timeout);
+}
+
+fn(fn);
